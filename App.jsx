1a27d0267c933c46bc8b0c7256089e13d519168b
msbah1 <mserehbah@gmail.com>
2023-06-18 18:16:43 -0400

    added components and screens(Home, Login, Signup) folder, also setup navigation(stack)

--- App.jsx
+++ App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from './screens/Home';
 
+const {Navigator, Screen} = createNativeStackNavigator();
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <NavigationContainer>
+        <Navigator>
+          <Screen name="home" component={Home} />
+        </Navigator>
+      </NavigationContainer>
     </View>
   );
 };
